5daf99f57f473f62ad57aa02934b349dd6cf9d50
manglusia <stasiek.rupala@gmail.com>
2024-06-10 22:08:41 +0200

    changes

--- CW10/CodeFirst/Data/ApplicationContext.cs
+++ CW10/CodeFirst/Data/ApplicationContext.cs
@@ -24,12 +24,92 @@ public class ApplicationContext : DbContext
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.Entity<PrescriptionMedicament>().HasKey(pm => new { pm.IdMedicament, pm.IdPrescription });
-        
-        modelBuilder.Entity<Medicament>().ToTable("Medicaments");
-        modelBuilder.Entity<Prescription>().ToTable("Prescriptions");
-        modelBuilder.Entity<PrescriptionMedicament>().ToTable("PrescriptionMedicaments");
-        modelBuilder.Entity<Doctor>().ToTable("Doctors");
-        modelBuilder.Entity<Patient>().ToTable("Patients");
 
+        modelBuilder.Entity<Medicament>().HasData(new List<Medicament>
+        {
+            new Medicament
+            {
+                Description = "Na wszytsko",
+                IdMedicament = 1,
+                Name = "Superix",
+                Type = "Przeciwbólowy"
+            },
+            new Medicament
+            {
+                Description = "Na noge",
+                IdMedicament = 2,
+                Name = "Nogix",
+                Type = "Przeciwzapalny"
+            }
+        });
+        modelBuilder.Entity<Prescription>().HasData(new List<Prescription>
+        {
+            new Prescription
+            {
+                Date = DateTime.Parse("2024-05-28"),
+                DueDate = DateTime.Parse("2024-05-30"),
+                IdDoctor = 1,
+                IdPatient = 1,
+                IdPrescription = 1,
+            },
+            new Prescription
+            {
+                Date = DateTime.Parse("2024-06-28"),
+                DueDate = DateTime.Parse("2024-06-30"),
+                IdDoctor = 2,
+                IdPatient = 2,
+                IdPrescription = 2,
+            }
+        });
+        modelBuilder.Entity<PrescriptionMedicament>().HasData(new List<PrescriptionMedicament>
+        {
+            new PrescriptionMedicament
+            {
+                Details = "Brac rano i wieczorem",
+                Dose = 2,
+                IdMedicament = 2,
+                IdPrescription = 1,
+            },
+            new PrescriptionMedicament
+            {
+                Details = "Kiedykolwiek",
+                Dose = 1,
+                IdMedicament = 1,
+                IdPrescription = 2,
+            }
+        });
+
+        modelBuilder.Entity<Doctor>().HasData(new List<Doctor>
+        {
+            new Doctor
+            {
+                Email = "superDoktorek@gmail.com",
+                FirstName = "Doktorek",
+                LastName = "Super",
+                IdDoctor = 1
+            },   
+            new Doctor
+            {
+                Email = "slabyDoktorek@wp.pl",
+                FirstName = "Doktorek",
+                LastName = "Slaby",
+                IdDoctor = 2
+            }
+        });
+        modelBuilder.Entity<Patient>().HasData(new List<Patient>
+        {
+            new Patient
+            {
+                BirthDate = DateTime.Parse("26.08.2004"),
+                FirstName = "Klaus",
+                LastName = "Nowak"
+            },
+            new Patient
+            {
+                BirthDate = DateTime.Parse("6-07-2010"),
+                FirstName = "Michał",
+                LastName = "Truba"
+            }
+        });
     }
 }
